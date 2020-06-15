d6567354c784fa2977bac28c0e7743e3f0ecc736
kyori19 <kyori@accelf.net>
2020-06-15 11:23:22 +0900

    Update dependencies

--- easter/src/main/java/net/accelf/easter/AccelForceEasterView.java
+++ easter/src/main/java/net/accelf/easter/AccelForceEasterView.java
@@ -2,8 +2,8 @@ package net.accelf.easter;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatImageView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Toast;
